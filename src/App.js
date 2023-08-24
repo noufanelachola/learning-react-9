a22b705276104378d929eb76af88ad5300be9daf
noufanelachola <noufansadiqelachola@gmail.com>
2023-08-24 07:20:36 +0530

    spi called and linke with the button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React , {useState} from "react";
+
+import React , {useState , useEffect} from "react";
+
 function App() {
-  const [count,setCount] = useState(0);
+
+  const [count,setCount] = useState(1);
+  const [data,setData] = useState(0);
+
   function handleclick(){
     setCount(prevCount => prevCount + 1)
   }
+
+  useEffect(() => {
+    console.log("effect ran");
+    fetch(`https://swapi.dev/api/people/${count}`)
+      .then(resp => resp.json())
+      .then(data => setData(data));
+  },[count])
+
   return (
-    <button onClick={handleclick} style={{padding:"2rem 1.5rem"}}>`You clicked me ${count} times`</button>
-);
+    <div>
+      <button onClick={handleclick} style={{padding:"2rem 1.5rem"}}>`You clicked me ${count} times`</button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+
 }
 
 export default App;
